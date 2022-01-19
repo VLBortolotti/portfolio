2bcb8e7066f78b271521f819f2b78a4eb0397aba
VLBortolotti <vitorlacobortolotti@gmail.com>
2022-01-19 20:39:53 -0300

    Menu responsivo

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const hamburgerButton = document.getElementById("hamburger");
-const navList = document.getElementById("nav");
+const navList = document.getElementById("menu-ul");
 
 function toggleButton() {
 	navList.classList.toggle('show');
