6483f84457838448f8d0a608e66bb0de07fc1725
VLBortolotti <vitorlacobortolotti@gmail.com>
2022-01-19 22:39:25 -0300

    botao ir pra cima add

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Menu hamburgr responsivo
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("menu-ul");
 
@@ -6,3 +7,10 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+// Seta voltar pra cima
+const btnScrollToTop = document.getElementById("btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function() {
+	window.scrollTo(0, 0);
+});
